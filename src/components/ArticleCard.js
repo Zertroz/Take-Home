94424407ccbcfb4f2a66eca4c549f7c7adeb81a7
Kass Daniello <zertroz@gmail.com>
2023-05-31 18:10:14 -0400

    Add styling to article cards and homepage

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,10 +1,14 @@
+import './ArticleCard.css'
+
 export const ArticleCard = ({title, description, date, image}) => {
   return (
     <div className="article-card">
       <img src={image} />
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <p>{date}</p>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <p>{date}</p>
+      </div>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import { ArticleCard } from "../components/ArticleCard"
+import { Form } from "../components/Form"
+import './Home.css'
 
 export const HomePage = ({articles}) => {
   const cards = articles.map(article => <ArticleCard key={article.title} title={article.title} description={article.description} image={article.urlToImage} date={article.publishedAt} />)
@@ -6,8 +8,18 @@ export const HomePage = ({articles}) => {
   console.log(cards)
 
   return (
-    <div>
-      {cards}
-    </div>
+    <section className="home-page">
+      <header>
+        <h1>Website Title</h1>
+      </header>
+      <section className="body">
+        <div className="form-container">
+          <Form />
+        </div>
+        <div className="card-container">
+          {cards}
+        </div>
+      </section>
+    </section>
   )
 }
